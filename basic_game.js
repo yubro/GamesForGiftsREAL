54498d837c14c0e3bf288aacaa15f08be7c7a6d2
Tom Knapp-Ramos <tomknappramos@gmail.com>
2017-03-11 11:48:06 -1000

    -a

---
+++ basic_game.js
@@ -0,0 +1,55 @@
+var game;
+var ship;
+var text;
+
+// socket connection
+const socket = io();
+
+//  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
+//  Although it will work fine with this tutorial, it's almost certainly not the most current version.
+//  Be sure to replace it with an updated version before you start experimenting with adding your own code.
+
+game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser_container', { preload: preload, create: create });
+
+function preload () {
+
+  // load images
+  game.load.image('logo', 'phaser.png');
+  game.load.image('mc', 'mc.png');
+
+}
+
+function create () {
+
+  ship = game.add.sprite(game.world.centerX, game.world.centerY, 'mc');
+  ship.anchor.setTo(0.5, 0.5);
+  ship.scale.setTo(0.5, 0.5);
+
+  //	Progress report
+  text = game.add.text(32, 32, 'Click to start load', { fill: '#FFFFFF' });
+
+  //	You can listen for each of these events from Phaser.Loader
+  game.load.onLoadStart.add(loadStart, this);
+  game.load.onLoadComplete.add(loadComplete, this);
+
+}
+
+function loadStart() {
+
+}
+
+function loadComplete() {
+      ship.loadTexture('mc',0,true);
+}
+
+myFunction = function() {
+
+  // change mc.png file with other file
+  swapShipTexture();
+
+}
+
+swapShipTexture = function() {
+  game.load.image('mc', 'red_hat.png');
+  game.load.start();
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+/*
+built using node.js, angular, and phaser
+*/
+
+
+// usual requires
+const express = require('express');
+const app = express();                    // express is just callback functions
+const http = require('http').Server(app); // connect express callbacks with this server :)
+const io = require('socket.io')(http);    // get sockets capability
+
+const fs = require('fs');
+const request = require('request');
+
+// download function
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download('http://www.pngall.com/wp-content/uploads/2016/03/Penguin-Download-PNG.png', 'mc.png', function(){
+  console.log('done');
+});
+
+// test
+/*
+fs.createReadStream(__dirname + '/mc.png').pipe(fs.createWriteStream(__dirname + '/mc2.png'));
+*/
+
+// usual get local dirs
+app.use(express.static(__dirname)); // add to remote folder
+
+//////////////////
+
+app.get('/', function(req, res){                // request and response
+  res.sendFile(__dirname + '/index.html');
+});
+
+////////////////////
+
+// not working for some reason...
+io.on('connection', function(socket){
+
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){          // disconnect handling
+    console.log('user disconnected');
+  });
+
+});
+
+
+http.listen(3000, function(){              // return a new listening http server
+  console.log('listening on *:3000');
+});
